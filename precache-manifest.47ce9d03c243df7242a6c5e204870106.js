542008636e3c9eece1b9a8f1961e8b9dbff1cc47
Adam <adam.nowakiewicz@nethone.com>
2020-04-26 21:55:07 +0200

    Updates

---
+++ precache-manifest.47ce9d03c243df7242a6c5e204870106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11263143867d88de0f2a86c99e91aa59",
+    "url": "/enterprise-dashboard/index.html"
+  },
+  {
+    "revision": "0a2b265a54714e1c9661",
+    "url": "/enterprise-dashboard/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "483d1bff3bd6b26283f8",
+    "url": "/enterprise-dashboard/static/css/main.958411ba.chunk.css"
+  },
+  {
+    "revision": "0a2b265a54714e1c9661",
+    "url": "/enterprise-dashboard/static/js/2.0efdf7d2.chunk.js"
+  },
+  {
+    "revision": "aa461ca1e806d5d1e3613e67dcfe6f98",
+    "url": "/enterprise-dashboard/static/js/2.0efdf7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483d1bff3bd6b26283f8",
+    "url": "/enterprise-dashboard/static/js/main.2f2a2845.chunk.js"
+  },
+  {
+    "revision": "b3199d01396d14c46760",
+    "url": "/enterprise-dashboard/static/js/runtime-main.7ae27f99.js"
+  }
+]);
